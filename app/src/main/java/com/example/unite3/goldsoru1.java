1250ad8bd16624c03dbd5c4c81635c9781c361c6
BIL-LAB2PC00\Ogrenci <seyydinburhanedin.mtal38@gmail.com>
2023-10-25 11:51:18 +0300

    no message

---
+++ app/src/main/java/com/example/unite3/goldsoru1.java
@@ -0,0 +1,14 @@
+package com.example.unite3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class goldsoru1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goldsoru1);
+    }
+}
---
+++ app/src/main/java/com/example/unite3/soru2.java
@@ -0,0 +1,14 @@
+package com.example.unite3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class soru2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_soru2);
+    }
+}
